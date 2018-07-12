70678f1a5dd887a6d0bf0a9d96fc502b09a4b561
米山杏里 <Anri@yoneyamaanri-no-macbook-air.local>
2018-07-12 18:58:51 +0900

    Initial Commit

---
+++ Smaphonika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smaphonika
+//
+//  Created by 米山杏里 on 2018/07/12.
+//  Copyright © 2018年 jp.yoneyamaanri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
